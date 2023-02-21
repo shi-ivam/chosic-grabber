6cb101f8794d2161d228db0be0b723fd78b4907e
Shivam Kumar <savagedeveloper795@gmail.com>
2023-02-21 22:55:34 +0530

    v2

--- snippets.js
+++ snippets.js
@@ -1,3 +1,6 @@
 
-// GRAB ALL NAMES
-[...document.querySelectorAll(".track-list-item-info-text")].map(e => e.querySelector('a')).map(e => e.innerText)
+// GRAB ALL NAMES (ONLY WORKS ON 100 LIST, NOT MORE NOT LESS)
+x = []
+x = [...document.querySelectorAll(".track-list-item-info-text")].slice(0,99).map(e =>( e.querySelector('div a').innerText + ' - ' + e.querySelector(".track-list-item-info-genres").innerText))
+// COPY TO CLIPBOARD
+copy(x.join("\n"))
